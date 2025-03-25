dea3b20b24349e0b38c6ab41a05152e6bdbbb36f
Pedro <pedrovalentepaulino26@gmail.com>
2025-03-25 10:08:20 -0300

    Básico do básico

---
+++ olamundo.py
@@ -0,0 +1,20 @@
+print("Olá, Mundo!")
+
+var1 = 10
+var2 = 20  
+
+# Comentario 
+
+if var1 > var2:
+    print("var1 é maior que var2")
+else:
+    print("var2 é maior que var1")
+
+var3 = (var1 + var2) * 2
+
+print(var3)
+
+"""
+Comentario de multiplas linhas
+"""
+ 
