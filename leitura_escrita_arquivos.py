00bb143c3672913528a33f7cf8a4be4dfdaa9c2b
Pedro <pedrovalentepaulino26@gmail.com>
2025-03-26 11:13:31 -0300

    Escrevendo em um arquivo externo

--- leitura_escrita_arquivos.py
+++ leitura_escrita_arquivos.py
@@ -1,3 +1,7 @@
 arquivo = open("arquivo.txt", "r")  
 print(arquivo.read())
 arquivo.close()
+
+arquivo = open("arquivo.txt", "w")  
+arquivo.write("Que mora dentro do meu coração --- Texto adiconado pelo programa")
+arquivo.close()
