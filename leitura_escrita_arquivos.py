76e2d6b31a5e69eb1877257df616b6158d33b8cf
Pedro <pedrovalentepaulino26@gmail.com>
2025-03-26 11:17:28 -0300

    with

--- leitura_escrita_arquivos.py
+++ leitura_escrita_arquivos.py
@@ -5,3 +5,6 @@ arquivo.close()
 arquivo = open("arquivo.txt", "w")  
 arquivo.write("Que mora dentro do meu coração --- Texto adiconado pelo programa")
 arquivo.close()
+
+with open("arquivo.txt", "r") as arquivo: 
+    print(arquivo.read())
