775b9465f1baf50d34f2d24361efb63e0d41885c
PedroValente <114920889+PedroValente21@users.noreply.github.com>
2025-03-27 09:21:27 -0300

    Delete tratamento_erro.py

--- tratamento_erro.py
+++
@@ -1,9 +0,0 @@
-def funcao():
-    variavel = 1
-    if variavel == 1:
-        raise Exception("Pode ser um não doidão")
-
-try:
-    funcao()
-except Exception as e:
-    print("Deu pau esse trein aqui ó:", e)
